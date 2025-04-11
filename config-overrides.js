2c77608a1658c2ee5ec4dfe0962c189957145e9b
pavansharan007 <narampavan32@gmail.com>
2025-04-11 21:19:54 +0530

    added few changes

---
+++ config-overrides.js
@@ -0,0 +1,18 @@
+// config-overrides.js
+const webpack = require("webpack");
+
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
+  };
+  config.plugins = [
+    ...config.plugins,
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"],
+    }),
+  ];
+  return config;
+};
